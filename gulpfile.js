202b348f42abac61da90f1b91f08eeeb4a6adc8d
nataSokolnikova <167069028+nataSokolnikova@users.noreply.github.com>
2025-10-14 18:08:04 +0300

    new

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/sass/*.+(scss|sass)')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({
+      prefix: "",
+      suffix: ".min"
+    }))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/sass/*.+(scss|sass)', gulp.parallel('styles'))
+  gulp.watch('src/sass/*.html').on('change',browserSync.reload)
+})
+
+gulp.task('default',gulp.parallel('watch','server','styles'))
